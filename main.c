91028f72518d2c4b7415bd459a1517d17078d478
Nilipta <niliptasatapathy@gmail.com>
2019-06-11 23:37:17 +1100

    Atmega8-lcd print i2c pcf8574

--- main.c
+++ main.c
@@ -1,28 +1,30 @@
 /*
- * blink.c
+ * GccApplication1.c
  *
- * Created: 6/9/2019 8:30:24 PM
+ * Created: 6/10/2019 12:04:20 AM
  * Author : prouser
  */ 
 
-#ifndef F_CPU
 #define F_CPU 16000000UL
-#endif
 
 #include <avr/io.h>
 #include <util/delay.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "I2C_LCD.h"
 
 
 int main(void)
 {
+	char buffer[20];
+	I2C_LCD_init();
+	I2C_LCD_write_string_XY(0, 0, "hello world");
+	I2C_LCD_write_string_XY(1, 1, "hello world");
+	
+	
     /* Replace with your application code */
-	DDRD = 0XFF;
     while (1) 
     {
-		PORTD = 0XFF;
-		_delay_ms(1000);
-		PORTD = 0X00;
-		_delay_ms(1000);
     }
 }
 
