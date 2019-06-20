90d89c8b95c5b22da857e4dc94b1fe9b24958750
Nilipta <niliptasatapathy@gmail.com>
2019-06-20 22:41:58 +1100

    atmega8 Print to lcd when serial receive

--- main.c
+++ main.c
@@ -13,129 +13,146 @@
 #include <stdio.h>
 #include <avr/interrupt.h>
 #include "I2C_LCD.h"
+
 #define USART_BAUDRATE 9600
 #define BAUD_PRESCALE (((F_CPU / (USART_BAUDRATE * 16UL))) - 1)
 
+static volatile long counterVal=0;
+static volatile char buffer[10]={"0"};
+static volatile uint8_t status = 0;
 
-static volatile long  counterVal = 0;
-
+void uart_transmit (unsigned char data);
 
 int main(void)
 {
 	
 	I2C_LCD_init();
-	
-	//UCSR0B = (1 << RXEN0) | (1 << TXEN0);   // Turn on the transmission and reception circuitry
-	//UCSR0C = 0x03; // Use 8-bit character sizes
-	
-	UCSR0B |= (1<<RXEN0)  | (1<<TXEN0);
-	UCSR0C |= (1<<UCSZ00) | (1<<UCSZ01);
-	UBRR0H  = (BAUD_PRESCALE >> 8);
-	UBRR0L  = BAUD_PRESCALE;
-	//UBRR0H = (BAUD_PRESCALE >> 8); // Load upper 8-bits of the baud rate value into the high byte of the UBRR register
-	//UBRR0L = BAUD_PRESCALE;
 	_delay_ms(5);
-	int index = 0;
-	char serialBuf[10]=" ";
-	char xx;
+	/************TIMER****************************/
+	//timer interrupt 1 second
+	OCR1A = 15624;
+
+	TCCR1B |= (1 << WGM12);
+	// Mode 4, CTC on OCR1A
+
+	TIMSK |= (1 << OCIE1A);
+	//Set interrupt on compare match
+
+	TCCR1B |= (1 << CS12) | (1 << CS10);
+	// set pre-scaler to 1024 and start the timer
 	
-	I2C_LCD_write_string_XY(1, 0, "0");
-	do{
-		while( ( UCSR0A & ( 1 << RXC0 ) ) == 0 );
-		xx = UDR0;
-		while (1) 
+	/************TIMER END****************************/
+	
+	/************ SERIEL ****************************/
+	
+	UCSRB = (1 << RXEN) | (1 << TXEN);   // Turn on the transmission and reception circuitry
+	//UCSRB = (1<<RXCIE) | (1 << RXEN) | (1 << TXEN);
+	UCSRC = (1 << URSEL) | (1 << UCSZ0) | (1 << UCSZ1); // Use 8-bit character sizes
+
+	UBRRH = (BAUD_PRESCALE >> 8); // Load upper 8-bits of the baud rate value into the high byte of the UBRR register
+	UBRRL = BAUD_PRESCALE;
+	_delay_ms(5);
+	
+	/************ SERIEL END****************************/
+	
+	
+	sei();
+	// enable interrupts
+
+	
+	
+    while (1) 
+    {
+		I2C_LCD_write_string_XY(0, 0, "sending Number - ");
+		_delay_ms(1000);
+		//if(counterVal%2==0)
 		{
-			I2C_LCD_write_string_XY(0, 0, "Last Value =");
-			//I2C_LCD_write_string_XY(1, 0, "hello world");
-			// wait until a byte is ready to read
-			while( ( UCSR0A & ( 1 << RXC0 ) ) == 0 )
-			{
+			long tempI=0;
+			char tempBuf[10];
+			sprintf (buffer, "%lu", counterVal);
+			while(buffer[tempI]!='\0'){
+				
+				//sprintf (tempBuf, "%lu", tempI);
+				//I2C_LCD_write_string_XY(0, 0, tempBuf);
+				uart_transmit(buffer[tempI]);
+				//uart_transmit('X');
+				tempI++;	//checking how many digit in counterval
 			}
-		
-			// grab the byte from the serial port
-		
-
-
-			/*while(serialBuf[index] == '\n')
+			
+			if(buffer[tempI]=='\0')
 			{
-					serialBuf[index] = UDR0;
-					index ++;	
-			}*/
-			//sprintf(serialBuf, "%d",index );
-		
-		
-			char x = UDR0;	
+				
+				uart_transmit('\n');
+				_delay_ms(400);
+				//check for response....
+				checkResponseFor5Sec();
+			}
 			
 				
-		
-			if(x != '\n')
-			{
-		
-			serialBuf[index] =x;
-			index++;	
-			
-			}
-			
-			if(x == '\n')
-			{
-				I2C_LCD_write_string_XY(1, 0, serialBuf);
-				index = 0;
-				_delay_ms(350);
-				sendResponse();
-			}
-				
-		
-			/*char resp[3]={'o','k','|'};
-			for(uint8_t index = 0;index <3;index++)
-			{
-				// wait until the port is ready to be written to
-				while( ( UCSR0A & ( 1 << UDRE0 ) ) == 0 ){}
-
-				// write the byte to the serial port
-				UDR0 = resp[index];
-			
-			}*/
-		
-			//sprintf (buffer, "%lu", counterVal);
-			//I2C_LCD_write_string_XY(0, 0, "hello world");
-			//_delay_ms(2000);
-			//I2C_LCD_write_string_XY(1, 0, buffer);
-			//_delay_ms(2000);
-			//counterVal++;
-			/* Replace with your application code */
-		}
-	}while(xx != '\n');
-	
+		}	
+    }
 }
 
 
-void sendResponse()
+ISR (TIMER1_COMPA_vect)
 {
-	I2C_LCD_write_string_XY(0, 0, "Response = =");
-	_delay_ms(300);
-	char resp[4] = "ok|";
-	for(uint8_t i = 0; i< 3; i++){
-		// wait until the port is ready to be written to
-		while( ( UCSR0A & ( 1 << UDRE0 ) ) == 0 ){}
+	// action to be done every 1 sec
+	counterVal++;
+	sprintf (buffer, "%lu", counterVal);
+	I2C_LCD_write_string_XY(1, 0, buffer);
+}
 
-		// write the byte to the serial port
-		UDR0 = resp[i];
+// function to send data
+void uart_transmit (unsigned char data)
+{
+	while (!( UCSRA & (1<<UDRE)));                // wait while register is free
+	UDR = data;                                   // load data in the register
+}
+
+void checkResponseFor5Sec()
+{
+	I2C_LCD_write_string_XY(0, 0,"Response??  ");
+	char x[10];
+	uint8_t checkInd = 0;
+	while(1)
+	{
+		while (!(UCSRA & (1 << RXC)) )
+		{			
+		}
+		 ; // Do nothing until data have been received and is ready to be read from UDR
+		x[checkInd] = UDR;
+		checkInd++;
+		
+		if(x[checkInd] == '|' || x[checkInd] == '\n' || x[checkInd] == '\0')
+		{
+			for(uint8_t ir =0; ir<10; ir++)
+			{
+				if(x[ir] == 'o' || x[ir] == 'k' || x[ir] == '|')
+					{
+						I2C_LCD_write_string_XY(0, 0, " --   ok   --  ");
+						break;		
+					}
+				else
+				{
+					I2C_LCD_write_string_XY(0, 0, "-- NAN -- ");
+					break;
+				}
+			}
+		}
+		break;	
+		
 	}
+	
+	_delay_ms(1000);
 }
 
 
-/*
+/*ISR ( USART_RXC_vect )
 
-// wait until a byte is ready to read
-while( ( UCSR0A & ( 1 << RXC0 ) ) == 0 ){}
+{
+	char x[5] = "Hello";
 
-// grab the byte from the serial port
-recieved_byte = UDR0;
+	x[1] = UDR ; // Fetch the received byte value into the variable " ByteReceived "
 
-// wait until the port is ready to be written to
-while( ( UCSR0A & ( 1 << UDRE0 ) ) == 0 ){}
-
-// write the byte to the serial port
-UDR0 = recieved_byte;
-
-*/
+	I2C_LCD_write_string_XY(0, 0, x);
+}*/
